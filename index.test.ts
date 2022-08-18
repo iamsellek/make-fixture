5d41797b36efa139a8fbc7c904c025f3f7b76dd7
Chris Sellek <iamsellek@gmail.com>
2022-08-18 13:58:03 -0400

    Add the ability to generate multiple fixtures with just one function call.

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import { makeFixture } from './index';
+import { makeFixture, makeFixtures } from './index';
 
 interface FixtureTest {
   one: number;
@@ -6,26 +6,60 @@ interface FixtureTest {
   three: number;
 }
 
-const defaults: FixtureTest = {
+const getDefaults = (): FixtureTest => ({
   one: 1,
   two: 2,
   three: 3,
-};
+});
 
-describe('Fixture tests', () => {
+describe('makeFixture tests', () => {
   it('Overrides fixture values correctly', () => {
     const overrides: Partial<FixtureTest> = {
       two: 4,
     };
 
-    expect(makeFixture(defaults, overrides)).toEqual({
+    expect(makeFixture(getDefaults(), overrides)).toEqual({
       one: 1,
       two: 4,
       three: 3,
     });
   });
 
-  it('Returns the fixture if no overrides are provided', () => {
-    expect(makeFixture(defaults, undefined)).toEqual(defaults);
+  it('Returns the default fixture if no overrides are provided', () => {
+    expect(makeFixture(getDefaults(), undefined)).toEqual(getDefaults());
+  });
+});
+
+describe('makeFixtures tests', () => {
+  it('Returns an array of fixtures of length count', () => {
+    expect(makeFixtures(getDefaults(), 3)).toEqual([
+      makeFixture(getDefaults()),
+      makeFixture(getDefaults()),
+      makeFixture(getDefaults()),
+    ]);
+  });
+
+  it('Returns an array of fixtures with each fixture overridden by the overrides', () => {
+    expect(makeFixtures(getDefaults(), 3, { two: 4 })).toEqual([
+      makeFixture(getDefaults(), { two: 4 }),
+      makeFixture(getDefaults(), { two: 4 }),
+      makeFixture(getDefaults(), { two: 4 }),
+    ]);
+  });
+
+  it('Returns an array of fixtures with the unique identifier unique for each object', () => {
+    expect(makeFixtures(getDefaults(), 3, undefined, 'two')).toEqual([
+      makeFixture(getDefaults(), { two: 0 }),
+      makeFixture(getDefaults(), { two: 1 }),
+      makeFixture(getDefaults(), { two: 2 }),
+    ]);
+  });
+
+  it('Returns an array of fixtures with the unique identifier unique for each object and overridden values', () => {
+    expect(makeFixtures(getDefaults(), 3, { one: 4 }, 'two')).toEqual([
+      makeFixture(getDefaults(), { one: 4, two: 0 }),
+      makeFixture(getDefaults(), { one: 4, two: 1 }),
+      makeFixture(getDefaults(), { one: 4, two: 2 }),
+    ]);
   });
 });
--- index.ts
+++ index.ts
@@ -2,3 +2,24 @@ export const makeFixture = <T>(defaults: T, overrides?: Partial<T>): T => ({
   ...defaults,
   ...overrides,
 });
+
+export const makeFixtures = <T>(
+  defaults: T,
+  count: number,
+  overrides?: Partial<T>,
+  uniqueIdentifierKey?: keyof T
+): T[] => {
+  const fixtures: T[] = [];
+
+  for (let i = 0; i < count; i++) {
+    if (uniqueIdentifierKey !== undefined) {
+      fixtures.push(
+        makeFixture(defaults, { ...overrides, [uniqueIdentifierKey]: i })
+      );
+    } else {
+      fixtures.push(makeFixture(defaults, overrides));
+    }
+  }
+
+  return fixtures;
+};
